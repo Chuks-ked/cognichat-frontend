fdf26329c5c6eb401d8b3d18d07de7c22b729827
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-07-20 02:08:45 +0100

    implement navbar

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom"
-import { SidebarProvider, SidebarTrigger } from "./ui/sidebar"
 import { AppSidebar } from "./AppSidebar"
-
+import { SidebarProvider } from "./ui/sidebar"
+import Navbar from "./Navbar"
 
 const MainLayout = () => {
     return (
         <SidebarProvider>
             <AppSidebar />
             <main className="w-full h-screen flex flex-col">
-                <SidebarTrigger />
+                <Navbar />
                 {<Outlet />}
             </main>
         </SidebarProvider>
