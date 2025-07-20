6097f702d9415fb4d8578a4a92a348064a51ae70
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-07-20 02:26:57 +0100

    import updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
-import HomePage from "./pages/HomePage";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="chats/:chat_uid" element={<HomePage />} />
-          <Route path="chats/new" element={<HomePage />} />
+          <Route index element={<Home />} />
+          <Route path="chats/:chat_uid" element={<Home />} />
+          <Route path="chats/new" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
